0fabb66d83835cd981fe4e3f412bc072191dbecf
jtothepro <168333109+jtothepro@users.noreply.github.com>
2024-09-06 13:29:21 +0200

    Added js to handle the form and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  //let recipePlace = document.querySelector("#recipe-placeholder");
+  //recipePlace.innerHTML = "JS Test";
+
+  new Typewriter("#recipe-placeholder", {
+    strings: ["Generating Recipe..."],
+    autoStart: true,
+    cursor: "",
+  });
+}
+let recipeFormElement = document.querySelector("#form");
+recipeFormElement.addEventListener("submit", generateRecipe);
